d6b1395684a330783fcba5c83ceccc01602e937f
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-04-19 00:30:15 +0900

    [feat] 랜딩페이지 UI 구현 완료

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Outer from "./pages/Outer";
 import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import PhoneCertify from "./pages/PhoneCertify";
 
 const Router = () => {
   return (
@@ -9,6 +11,8 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Outer />}>
           <Route path="" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/phoneCertify" element={<PhoneCertify />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,91 @@
 import React from "react";
 import styled from "styled-components";
+import landingBackground from "../assets/landing/LandingBackground.svg";
+import rigthArrow from "../assets/landing/RightArrow.svg";
+import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
-  return <TotalComponent>랜딩페이지</TotalComponent>;
+  const navigate = useNavigate();
+
+  const onClickArrow = () => {
+    navigate("/login");
+  };
+
+  return (
+    <>
+      <LandingTotalComponent>
+        <LandingBackgroundComponent src={landingBackground} alt="background" />
+        <Header>
+          투명한 노동 시장,
+          <br />
+          직통에서 시작됩니다
+        </Header>
+        <Content>
+          과도한 중간 수수료, 임금 체불 사업주,
+          <br />
+          위험한 근로환경은 더 이상 그만
+        </Content>
+        <Footer>
+          <FooterText>시작하기</FooterText>
+          <FooterArrow onClick={onClickArrow} src={rigthArrow} alt=">" />
+        </Footer>
+      </LandingTotalComponent>
+    </>
+  );
 };
 
 export default Landing;
 
-const TotalComponent = styled.div``;
+const LandingTotalComponent = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: #2623d3;
+`;
+
+const LandingBackgroundComponent = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const Header = styled.div`
+  font-family: "Pretendard";
+  padding: 507px 0px 0px 0px;
+  color: #fff;
+  text-align: center;
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Content = styled.div`
+  font-family: "Pretendard Variable";
+  margin: 30px 0px 0px 0px;
+  color: #fff;
+  text-align: center;
+  font-size: 13px;
+  font-weight: 200;
+`;
+
+const Footer = styled.div`
+  margin: 100px 0px 0px 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FooterText = styled.div`
+  font-family: "Pretendard";
+  color: #fff;
+  text-align: center;
+  font-size: 30px;
+`;
+
+const FooterArrow = styled.img`
+  margin: 0px 0px 0px 8px;
+  width: 48px;
+  height: 49.647px;
+  z-index: 1;
+  cursor: pointer;
+`;
