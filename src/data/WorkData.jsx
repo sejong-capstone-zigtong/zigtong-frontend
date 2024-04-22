2a1048c8ed83451f0f32797ac08898c492994e1a
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-04-22 20:36:12 +0900

    [feat] 아래 공통 Footer 구현

---
+++ src/data/WorkData.jsx
@@ -0,0 +1,19 @@
+export const WorkList = [
+  { value: "전체", label: "전체" },
+  { value: "건설", label: "건설" },
+  { value: "주방", label: "주방" },
+  { value: "청소", label: "청소" },
+  { value: "가사", label: "가사" },
+  { value: "행사/판촉", label: "행사/판촉" },
+  { value: "물류", label: "물류" },
+  { value: "유통", label: "유통" },
+];
+
+export const FilterList = [
+  { value: "최신순", label: "최신순" },
+  { value: "근무가능일", label: "근무가능일" },
+  { value: "시간", label: "시간" },
+  { value: "급구", label: "급구" },
+  { value: "장기가능", label: "장기가능" },
+  { value: "초보가능", label: "초보가능" },
+];
