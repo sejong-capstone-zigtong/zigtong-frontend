52916fbc206e450ba140994ef36351bbeb55e1d6
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-05-17 20:37:30 +0900

    [fix] pages 폴더구조 변경

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,20 +6,20 @@ import Landing from "pages/common/Landing";
 // 로그인
 import Login from "pages/common/Login";
 // 회원가입
-import PhoneCertify from "pages/worker/PhoneCertify";
-import PhoneCertifyConfirm from "pages/worker/PhoneCertifyConfirm";
-import Terms from "pages/worker/Terms";
-import OtherInfo from "pages/worker/OtherInfo";
+import PhoneCertify from "pages/worker/signup/PhoneCertify";
+import PhoneCertifyConfirm from "pages/worker/signup/PhoneCertifyConfirm";
+import Terms from "pages/worker/signup/Terms";
+import OtherInfo from "pages/worker/signup/OtherInfo";
 // 일자리 찾기
-import SearchWork from "pages/worker/SearchWork";
-import WorkDetail from "pages/worker/WorkDetail";
+import SearchWork from "pages/worker/works/SearchWork";
+import WorkDetail from "pages/worker/works/WorkDetail";
 // 일자리 배정
-import Assignment from "pages/worker/Assignment";
+import Assignment from "pages/worker/works/Assignment";
 // 찜한 일자리
-import SavedWorks from "pages/worker/SavedWork";
+import SavedWorks from "pages/worker/works/SavedWork";
 // 프로필
-import Profile from "pages/worker/Profile";
-import MyPage from "pages/worker/MyPage";
+import Profile from "pages/worker/profile/Profile";
+import MyPage from "pages/worker/profile/MyPage";
 
 const Router = () => {
   return (
